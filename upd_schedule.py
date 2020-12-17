47d234ff5a1bb260ce5d5e04050df2eaa98b97e2
Heinrich Hartmann <derhein@gmail.com>
2020-12-17 14:06:16 +0100

    Set update interval to 1h

--- upd_schedule.py
+++ upd_schedule.py
@@ -2,9 +2,11 @@ import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def exec_interval():
+    print("Starting update ...")
     result = subprocess.run(["pip", "install", "-U", "youtube-dl"])
-    print('exec subprocess 1day', result)
+    print("done:", result)
+
 sched = BlockingScheduler()
-sched.add_job(exec_interval, 'interval', seconds=1440)
+sched.add_job(exec_interval, 'interval', seconds=60*60)
 
 sched.start()
