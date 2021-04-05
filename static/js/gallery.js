2a1fd2543a60c0a6a2ee73e1dd93c2b35bd19c1b
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2021-04-05 13:39:16 +0200

    Match partials

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,27 +1,37 @@
 function L(x) { console.log(x); };
 function gallery_show(pattern) {
-    const matches_max = 16;
-    var matches = 0;
-    $("#grid .vid").each(function(index, rec) {
-        var text = $(rec).find(".vid-title").text().replace(/\s+/g, ' ').toLowerCase();
-        if (!~text.indexOf(pattern)) {
-            rec.style.display = "none";
-        } else {
-            matches++;
-            if(matches > matches_max) { return false; };
-            rec.style.display = "block";
-            let video = $(rec).find("video");
-            video.attr("poster", video.attr("data-poster"));
-        }
+  const matches_max = 16;
+  var matches = 0;
+  const fragments = pattern.split(/[ ]+/);
+  function pmatch(text) {
+    return fragments.every((pat) => {
+      if (!~text.indexOf(pat)) {
+        return false;
+      } else {
+        return true;
+      }
     });
+  }
+  $("#grid .vid").each(function(i,rec) { rec.style.display = "none"; });
+  $("#grid .vid").each(function(index, rec) {
+    var text = $(rec).find(".vid-title").text().replace(/\s+/g, ' ').toLowerCase();
+    if (pmatch(text)) {
+      matches++;
+      if(matches > matches_max) { return false; };
+      rec.style.display = "block";
+      let video = $(rec).find("video");
+      video.attr("poster", video.attr("data-poster"));
+    }
+    return 0;
+  });
 }
 
 $(() => {
-    // Initialize
-    gallery_show($('#search').val());
-    // Listen to change events
-    $('#search').keyup((event) => {
-        const pattern = event.currentTarget.value.replace(/ +/g, ' ').toLowerCase();
-        gallery_show(pattern);
-    });
+  // Initialize
+  gallery_show($('#search').val());
+  // Listen to change events
+  $('#search').keyup((event) => {
+    const pattern = event.currentTarget.value.replace(/ +/g, ' ').toLowerCase();
+    gallery_show(pattern);
+  });
 });
