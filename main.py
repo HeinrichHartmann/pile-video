78d5301bf7b1c146c8e79fdedbf956c39d38137b
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2024-11-02 16:14:38 +0100

    black

--- main.py
+++ main.py
@@ -26,9 +26,12 @@ import tempfile
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-logging.basicConfig(format="%(asctime)s logger=%(name)s lvl=%(levelname)s pid=%(process)d %(message)s")
+logging.basicConfig(
+    format="%(asctime)s logger=%(name)s lvl=%(levelname)s pid=%(process)d %(message)s"
+)
 L = logging.getLogger(__name__)
 L.setLevel(logging.INFO)
+
 
 def validate(env_var, default_path, create=False):
     path = Path(os.environ.get(env_var, default_path))
@@ -42,12 +45,13 @@ def validate(env_var, default_path, create=False):
             L.error(f"{env_var} does not exist: {path}")
     return path
 
+
 PVIDEOS = validate("VIDEOS", "./videos")
-PPILE= validate("PILE", "./videos/pile")
+PPILE = validate("PILE", "./videos/pile")
 PTRASH = validate("PTRASH", PVIDEOS / "trash/", create=True)
-PMP3= validate("MP3", "./mp3")
-PCACHE= validate("CACHE", "./cache")
-PTMP= validate("TMP", "./tmp")
+PMP3 = validate("MP3", "./mp3")
+PCACHE = validate("CACHE", "./cache")
+PTMP = validate("TMP", "./tmp")
 PDB = PCACHE / "pile.db"
 DEBUG = os.environ.get("DEBUG", False)
 
@@ -56,6 +60,7 @@ if DEBUG:
     L.debug("DEBUG=true")
 
 from concurrent.futures import Future
+
 
 def all_done_callback(futures, callback):
     """
@@ -68,11 +73,13 @@ def all_done_callback(futures, callback):
     """
     total_futures = len(futures)
     completed_futures = [0]  # Use a list for mutability
+
     def future_callback(fut: Future):
         nonlocal completed_futures
         completed_futures[0] += 1
         if completed_futures[0] == total_futures:
             callback()
+
     for f in futures:
         f.add_done_callback(future_callback)
 
@@ -81,14 +88,18 @@ def all_done_callback(futures, callback):
 # Thread Pool Management
 #
 
+
 class CallErrror(Exception):
     pass
+
 
 def call_sync(argv):
     cmd_line = " ".join(argv)
     cmd = argv[0]
     L.debug(f"Running `{cmd_line}`")
-    with subprocess.Popen(argv, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
+    with subprocess.Popen(
+        argv, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    ) as proc:
         # stderr is redirected stdout, so we see all messages here
         for line in proc.stdout:
             L.debug(f"{cmd} > " + line.decode("utf-8").rstrip("\n"))
@@ -98,6 +109,7 @@ def call_sync(argv):
         else:
             L.error(f"{cmd} > Failed running `{cmd_line}` with code {code}.")
             raise CallErrror(f"Failed running `{cmd_line}` with code {code}.")
+
 
 def call_capture(argv):
     cmd_line = " ".join(argv)
@@ -112,14 +124,19 @@ def call_capture(argv):
         raise e
     return completed_process.stdout.decode("utf-8")
 
+
 executor = ThreadPoolExecutor(max_workers=4)
+
+
 def exec_submit(fn, *args, **kwargs):
     future = executor.submit(fn, *args, **kwargs)
+
     def done_callback(future):
         if future.exception():
             L.error(f"Error in {fn.__name__}: {future.exception()}")
         else:
             L.debug(f"Done {fn.__name__}")
+
     future.add_done_callback(done_callback)
     return future
 
@@ -132,50 +149,114 @@ def exec_submit(fn, *args, **kwargs):
 #
 def generate_poster(path_src, path_dst):
     try:
-        call_sync([ "ffmpeg", "-n", "-v", "error", "-i", str(path_src), "-ss", "00:00:20.000", "-vframes", "1", str(path_dst) ])
+        call_sync(
+            [
+                "ffmpeg",
+                "-n",
+                "-v",
+                "error",
+                "-i",
+                str(path_src),
+                "-ss",
+                "00:00:20.000",
+                "-vframes",
+                "1",
+                str(path_dst),
+            ]
+        )
         assert path_dst.exists()
     except:
         L.error(f"Second try for {path_src}. Using first frame.")
-        call_sync([ "ffmpeg", "-n", "-v", "fatal", "-i", str(path_src), "-ss", "00:00:00.000", "-vframes", "1", str(path_dst) ])
+        call_sync(
+            [
+                "ffmpeg",
+                "-n",
+                "-v",
+                "fatal",
+                "-i",
+                str(path_src),
+                "-ss",
+                "00:00:00.000",
+                "-vframes",
+                "1",
+                str(path_dst),
+            ]
+        )
     assert path_dst.exists()
+
 
 def generate_duration(path):
     return float(
-        call_capture([ "ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", str(path) ])
+        call_capture(
+            [
+                "ffprobe",
+                "-v",
+                "error",
+                "-show_entries",
+                "format=duration",
+                "-of",
+                "default=noprint_wrappers=1:nokey=1",
+                str(path),
+            ]
+        )
     )
+
 
 def generate_audio(path_src, path_dst):
     L.debug(f"Extract audio {path_src}")
     with tempfile.TemporaryDirectory(dir=PTMP) as tmpdir:
         path_tmp: Path = Path(tmpdir) / "tmp.mp3"
-        call_sync([ "ffmpeg", "-i", str(path_src), str(path_tmp), ])
+        call_sync(
+            [
+                "ffmpeg",
+                "-i",
+                str(path_src),
+                str(path_tmp),
+            ]
+        )
         assert path_tmp.exists()
         shutil.move(path_tmp, path_dst)
     return path_dst.exists()
+
 
 def generate_recode(path_src, path_dst):
     L.info(f"Recode {path_src} -> {path_dst}")
     with tempfile.TemporaryDirectory(dir=PTMP) as tmpdir:
         path_tmp: Path = Path(tmpdir) / "tmp.mp4"
-        call_sync([ "ffmpeg", "-v", "fatal", "-n", "-i", str(path_src), "-c:a", "aac", str(path_tmp), ])
+        call_sync(
+            [
+                "ffmpeg",
+                "-v",
+                "fatal",
+                "-n",
+                "-i",
+                str(path_src),
+                "-c:a",
+                "aac",
+                str(path_tmp),
+            ]
+        )
         assert path_tmp.exists()
         shutil.move(path_tmp, path_dst)
     assert path_dst.exists()
-    path_src.unlink() # rm source
+    path_src.unlink()  # rm source
+
 
 def generate_download(url, output_dir):
     today = date.today().isoformat()
     L.info(f"Download {url}")
-    call_sync([
-        "yt-dlp",
-        "--no-progress",
-        "--restrict-filenames",
-        "--format",
-        "bestvideo[height<=760]+bestaudio",
-        "-o",
-        f"{output_dir}/{today} %(title)s via %(uploader)s.%(ext)s",
-        url,
-    ])
+    call_sync(
+        [
+            "yt-dlp",
+            "--no-progress",
+            "--restrict-filenames",
+            "--format",
+            "bestvideo[height<=760]+bestaudio",
+            "-o",
+            f"{output_dir}/{today} %(title)s via %(uploader)s.%(ext)s",
+            url,
+        ]
+    )
 
 
 def generate_keep_ytdl_updated():
@@ -188,6 +269,7 @@ def generate_keep_ytdl_updated():
         except Exception as e:
             L.error(f"Update yt-dlp failed: {e}")
         time.sleep(60 * 60 * 12)
+
 
 #
 # Video Data Class
@@ -208,24 +290,28 @@ class Video:
             name = m.group(1)
         return name
 
+
 #
 # Meta Database
 #
 import sqlite3
 import threading
+
+
 def db_connection():
     thread_local = threading.local()
-    if not hasattr(thread_local, 'sqlite_db'):
+    if not hasattr(thread_local, "sqlite_db"):
         thread_local.sqlite_db = sqlite3.connect(PDB)
         thread_local.sqlite_db.row_factory = sqlite3.Row
     return thread_local.sqlite_db
 
+
 def db_init():
     db = db_connection()
-    db.execute("""
+    db.execute(
+        """
         CREATE TABLE IF NOT EXISTS video (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            path TEXT UNIQUE,
+            path TEXT PRIMARY KEY,
             date TIMESTAMP,
             title TEXT,
             duration_sec INTEGER,
@@ -233,17 +319,23 @@ def db_init():
             poster_url TEXT,
             audio_path TEXT
         );
-    """)
+    """
+    )
     db.commit()
+
 
 def db_iter_videos():
     db = db_connection()
-    res = db.execute("SELECT path, title, duration_sec, video_url, poster_url, date FROM video ORDER BY date DESC")
+    res = db.execute(
+        "SELECT path, title, duration_sec, video_url, poster_url, date FROM video ORDER BY date DESC"
+    )
     for row in res:
         yield Video(dict(row))
 
 
-_re_date = re.compile("(\d\d\d\d\-\d\d-\d\d).*")
+_re_date = re.compile(r"(\d\d\d\d-\d\d-\d\d).*")
+
+
 def db_scan_videos(root: Path, prefix=""):
     VIDEO_EXT = {".mp4"}
     cnt = 0
@@ -261,23 +353,30 @@ def db_scan_videos(root: Path, prefix=""):
         date = None
         if m := _re_date.match(path.name):
             date = m.group(1)
-            title = path.name[len(date) + 1:]
-        db.execute("INSERT OR IGNORE INTO video (path, video_url, title, date) VALUES (?, ?, ?, ?)", (str(path), video_url, title, date))
+            title = path.name[len(date) + 1 :]
+        db.execute(
+            "INSERT OR IGNORE INTO video (path, video_url, title, date) VALUES (?, ?, ?, ?)",
+            (str(path), video_url, title, date),
+        )
         cnt += 1
     db.commit()
     L.info(f"Found {cnt} videos.")
+
 
 def set_duration(path: Path):
     duration = generate_duration(path)
     if duration:
         db = db_connection()
-        db.execute("UPDATE video SET duration_sec = ? WHERE path = ?", (duration, str(path)))
+        db.execute(
+            "UPDATE video SET duration_sec = ? WHERE path = ?", (duration, str(path))
+        )
         db.commit()
         L.debug(f"Set duration for {path} to {duration} seconds.")
         return True
     else:
         L.error(f"Failed to get duration for {path}")
     return False
+
 
 def db_scan_duration(prefix=""):
     db = db_connection()
@@ -291,17 +390,21 @@ def db_scan_duration(prefix=""):
         cnt += 1
     L.info(f"Queued {cnt} duration computations.")
 
+
 def set_poster(path: Path):
     L.debug(f"Generate poster {path}.")
     poster_path = PCACHE / (path.name + ".png")
     if generate_poster(path, poster_path):
         poster_url = "/poster/" + path.name + ".png"
         db = db_connection()
-        db.execute("UPDATE video SET poster_url = ? WHERE path = ?", (poster_url, str(path)))
+        db.execute(
+            "UPDATE video SET poster_url = ? WHERE path = ?", (poster_url, str(path))
+        )
         db.commit()
         L.debug(f"Set poster for {path} to {poster_url}.")
     else:
         L.error(f"Failed to generate poster for {path}.")
+
 
 def db_scan_poster(root: Path, prefix=""):
     db = db_connection()
@@ -316,27 +419,36 @@ def db_scan_poster(root: Path, prefix=""):
         if poster_path.exists():
             cnt_found += 1
             poster_url = "/poster/" + path.name + ".png"
-            db.execute("UPDATE video SET poster_url = ? WHERE path = ?", (poster_url, str(path)))
+            db.execute(
+                "UPDATE video SET poster_url = ? WHERE path = ?",
+                (poster_url, str(path)),
+            )
         else:
             cnt_queued += 1
             exec_submit(set_poster, path)
     db.commit()
     L.info(f"Found {cnt_found} posters. Queued {cnt_queued} poster generations.")
 
+
 def db_clear_posters():
     db = db_connection()
     db.execute("UPDATE video SET poster_url = NULL")
     db.commit()
+
 
 def db_clear_audio():
     db = db_connection()
     db.execute("UPDATE video SET audio_path = NULL")
     db.commit()
 
+
 def db_set_audio(path: Path, audio_path: Path):
     db = db_connection()
-    db.execute("UPDATE video SET audio_path = ? WHERE path = ?", (str(audio_path), str(path)))
+    db.execute(
+        "UPDATE video SET audio_path = ? WHERE path = ?", (str(audio_path), str(path))
+    )
     db.commit()
+
 
 def db_scan_audio(root, prefix=""):
     AUDIO_EXTRACT_EXT = {".mp4"}
@@ -350,7 +462,10 @@ def db_scan_audio(root, prefix=""):
         if not path.name.startswith(prefix):
             continue
         if audio_path.exists():
-            db.execute("UPDATE video SET audio_path = ? WHERE path = ?", (str(audio_path), str(path)))
+            db.execute(
+                "UPDATE video SET audio_path = ? WHERE path = ?",
+                (str(audio_path), str(path)),
+            )
             cnt_found += 1
         elif path.suffix in AUDIO_EXTRACT_EXT:
             cnt_queued += 1
@@ -359,6 +474,7 @@ def db_scan_audio(root, prefix=""):
             L.debug(f"Audio extraction skipped for {path}")
     db.commit()
     L.info(f"Found {cnt_found} audio files. Queued {cnt_queued} audio extractions.")
+
 
 def db_scan_recode(root: Path, prefix=""):
     VIDEO_RECODE_EXT = {".webm", ".mkv"}
@@ -378,11 +494,13 @@ def db_scan_recode(root: Path, prefix=""):
     L.info(f"Queued {cnt} videos for recoding.")
     return futures
 
+
 def db_video_remove(path: Path):
     db = db_connection()
     result = db.execute("DELETE FROM video WHERE path = ?", (str(path),))
     L.info(f"Removed {result.rowcount} from DB for {path}.")
     db.commit()
+
 
 #
 # Flask App
@@ -391,19 +509,23 @@ app = flask.Flask(__name__, static_folder="./static", template_folder="./templat
 
 socketio = flaskio.SocketIO(app, cors_allowed_origins="*")
 
+
 def websocket_send(msg):
     "Sends message to all known web-sockets"
     # L.debug(f"socket > {msg}")
     socketio.emit("message", msg, broadcast=True)
 
+
 class SocketLogHandler(logging.Handler):
     def emit(self, record):
         websocket_send(self.format(record))
+
 
 socket_formatter = logging.Formatter("%(asctime)s %(message)s", datefmt="%H:%M:%S")
 socket_log_handler = SocketLogHandler()
 socket_log_handler.setFormatter(socket_formatter)
 L.addHandler(socket_log_handler)
+
 
 @app.route("/download")
 def serve_download():
@@ -413,13 +535,14 @@ def serve_download():
 @app.route("/video/<path:filepath>")
 def serve_video(filepath):
     base_path = PVIDEOS.resolve
-    return flask.send_from_directory(PVIDEOS , filepath)
+    return flask.send_from_directory(PVIDEOS, filepath)
 
 
 @app.route("/audio/<path:filepath>")
 def serve_audio(filepath):
     base_path = PMP3.resolve
-    return flask.send_from_directory(PMP3 , filepath)
+    return flask.send_from_directory(PMP3, filepath)
+
 
 @app.route("/poster/<path:filepath>")
 def serve_poster(filepath):
@@ -456,19 +579,22 @@ def q_put():
     url = payload.get("url")
     # Nit: Callbacks are running 3 levels deep here. Better to make this a coro in the future.
     future = exec_submit(generate_download, url, PPILE)
+
     def cb(future):
-        future.result() # re-raise exception
+        future.result()  # re-raise exception
         # Hack
         # We don't have access to the exact file name of the download, but we know it start with today's date.
         # Hence, we re-scan files that start with todays date
         prefix = date.today().isoformat()
         recode_futures = db_scan_recode(PVIDEOS, prefix=prefix)
+
         def cbb():
-            future.result() # re-raise exception
+            future.result()  # re-raise exception
             db_scan_videos(PVIDEOS, prefix=prefix)
             db_scan_poster(PCACHE, prefix=prefix)
             db_scan_audio(PMP3, prefix=prefix)
             db_scan_duration(prefix=prefix)
+
         all_done_callback(recode_futures, cbb)
 
     future.add_done_callback(cb)
@@ -494,7 +620,8 @@ def serve_podcast():
     """
     xml_items = []
     for row in videos:
-        xml_items.append(f"""
+        xml_items.append(
+            f"""
         <item>
             <title>{row['title']}</title>
             <enclosure url="{flask.url_for('serve_audio', filepath=row['audio_path'], _external=True)}" type="audio/mpeg" />
@@ -503,19 +630,23 @@ def serve_podcast():
             <itunes:duration>{row['duration_sec']}</itunes:duration>
             <itunes:image href="{row['poster_url']}" />
             <itunes:subtitle>{row['title']}</itunes:subtitle>
-        </item>""")
+        </item>"""
+        )
     xml_tail = """</channel></rss>"""
     xml = xml_head + "\n".join(xml_items) + xml_tail
     return flask.Response(xml, mimetype="application/xml")
+
 
 @socketio.on("connect")
 def test():
     L.debug("Socket connected")
     socketio.send(f"Welcome!\n")
 
+
 @socketio.on("message")
 def handle_message(msg):
     L.debug(f"Socket received: {msg}")
+
 
 @click.command()
 @click.option("-p", "--port", default=8080, help="Port to listen on", envvar="PORT")
@@ -543,16 +674,19 @@ def main(port, quick):
     # app.config["TEMPLATES_AUTO_RELOAD"] = True
     # suppress werkzeug logging. For some reason we have to do this late in the process.
     logging.getLogger("werkzeug").setLevel(logging.WARNING)
-    socketio.run(app,
-                 host="0.0.0.0",
-                 port=port,
-                 debug=DEBUG,
-                 allow_unsafe_werkzeug=True,
-                 use_reloader=False) # BLOCK
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=DEBUG,
+        allow_unsafe_werkzeug=True,
+        use_reloader=False,
+    )  # BLOCK
 
     # CTRL-C will get us here.
     # Cleanup
     executor.shutdown()
+
 
 if __name__ == "__main__":
     main()
